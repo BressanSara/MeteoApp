aae3e281ed4eab1d4aac647932d17f8708aab11d
alessandro.formato <alessandro.formato@supsi.ch>
2025-03-22 17:04:55 +0100

    CREATE: Creata la classe di GPSOperations, che ritorna la posizione corrente.
    UPDATE: Aggiornati i manifest per chiedere il consenso delle posizioni; aggiornata la LocationListViewModel.cs mettendo la location corrente in prima posizione.

--- MeteoApp/Models/MeteoLocation.cs
+++ MeteoApp/Models/MeteoLocation.cs
@@ -6,9 +6,12 @@ using System.Threading.Tasks;
 
 namespace MeteoApp.Models
 {
-    class MeteoLocation
+    public class MeteoLocation
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
     }
 }
--- MeteoApp/ViewModels/LocationListViewModel.cs
+++ MeteoApp/ViewModels/LocationListViewModel.cs
@@ -5,7 +5,7 @@ namespace MeteoApp.ViewModels
 {
     class LocationListViewModel : BaseViewModel
     {
-        ObservableCollection<MeteoLocation> _locations;
+        private ObservableCollection<MeteoLocation> _locations;
 
         public ObservableCollection<MeteoLocation> Locations
         {
@@ -20,13 +20,24 @@ namespace MeteoApp.ViewModels
         public LocationListViewModel()
         {
             Locations = new ObservableCollection<MeteoLocation>();
+        }
 
-            for (var i = 0; i < 10; i++)
+        public async Task InitializeLocationsAsync()
+        {
+            GPSOperations gpsOperations = new GPSOperations();
+            var currentLocation = await gpsOperations.GetCurrentLocationAsync();
+            
+            if (currentLocation != null)
+                Locations.Insert(0, currentLocation);
+            
+            for (var i = 1; i < 10; i++)
             {
                 var e = new MeteoLocation
                 {
                     Id = i,
-                    Name = $"Location {i}"
+                    Name = $"Location {i}",
+                    Latitude = 0.0,
+                    Longitude = 0.0,
                 };
 
                 Locations.Add(e);
