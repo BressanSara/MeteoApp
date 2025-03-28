92b2481d085860975b49597fa4345af52773811a
Sara Bressan <sara.bressan@student.supsi.ch>
2025-03-28 09:28:07 +0100

    Revert "ADD: local secret (API_Key)"
    This reverts commit 0cf30e193075723455f4ff22dfe11c3a1d9bcc7d.

--- MeteoApp/Services/MeteoService.cs
+++ MeteoApp/Services/MeteoService.cs
@@ -1,30 +1,21 @@
 ﻿using MeteoApp.Models;
-using Microsoft.Extensions.Configuration;
 using System.Net.Http.Json;
 
 public class MeteoService
 {
     private readonly HttpClient _httpClient;
-    private string _apiKey;
+    private readonly string API_KEY = "4668180ff7117459af24747cfb00db9a";
 
     public MeteoService(HttpClient httpClient)
     {
         _httpClient = httpClient;
-
-        var builder = new ConfigurationBuilder()
-           .AddUserSecrets<MeteoService>();
-
-        var configuration = builder.Build();
-        _apiKey = configuration["OpenWeatherMapApiKey"];
-
     }
-
 
     public async Task<CurrentWeatherData> GetWeatherAsync(MeteoLocation location)
     {
         try
         {
-            var response = await _httpClient.GetAsync($"https://api.openweathermap.org/data/2.5/weather?lat={location.Coord.lat}&lon={location.Coord.lon}&appid={_apiKey}&units=metric");
+            var response = await _httpClient.GetAsync($"https://api.openweathermap.org/data/2.5/weather?lat={location.Coord.lat}&lon={location.Coord.lon}&appid={API_KEY}&units=metric");
 
             if (response.IsSuccessStatusCode)
             {
