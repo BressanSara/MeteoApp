315e2349ecb5d30f407c1e93d6715d3ef4687de0
alessandro.formato <alessandro.formato@supsi.ch>
2025-05-05 10:02:05 +0200

    CREATE: create le mappe e la loro pagina.

--- MeteoApp/MapPage.xaml.cs
+++ MeteoApp/MapPage.xaml.cs
@@ -1,3 +1,9 @@
+using System.ComponentModel;
+using Microsoft.Maui.Controls;
+using Map = Microsoft.Maui.Controls.Maps.Map;
+using Microsoft.Maui.Devices.Sensors;
+using Microsoft.Maui.Maps;
+
 namespace MeteoApp;
 
 public partial class MapPage : ContentPage
@@ -5,5 +11,12 @@ public partial class MapPage : ContentPage
 	public MapPage()
 	{
 		InitializeComponent();
+
+		var gps = new GPSOperations();
+
+		var current = gps.GetCurrentLocationAsync();
+		Map map = new Map();
+		Content = map;
+		
 	}
 }
